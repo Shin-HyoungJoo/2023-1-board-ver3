69620b5df1e4755739d2944cc5c9f11038f25de2
Administrator <tncjs3598@naver.com>
2023-05-30 16:49:11 +0900

    update

--- src/main/java/com/green/boardver3/BoardController.java
+++ src/main/java/com/green/boardver3/BoardController.java
@@ -1,4 +1,23 @@
 package com.green.boardver3;
 
+import com.green.boardver3.model.BoardInsDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
 public class BoardController {
+    private final BoardService service;
+@Autowired
+    public BoardController(BoardService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public int BoardService(@RequestBody BoardInsDto dto) {
+    return service.insBoard(dto);
+    }
 }
--- src/main/java/com/green/boardver3/BoardService.java
+++ src/main/java/com/green/boardver3/BoardService.java
@@ -1,4 +1,19 @@
 package com.green.boardver3;
 
+import com.green.boardver3.model.BoardInsDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardService {
+    private final BoardMapper mapper;
+
+    @Autowired
+    public BoardService(BoardMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public int insBoard(BoardInsDto dto) {
+        return mapper.insBoard(dto);
+    }
 }
