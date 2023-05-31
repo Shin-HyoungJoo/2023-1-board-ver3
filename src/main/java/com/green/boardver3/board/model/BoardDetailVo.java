b94cf24e03792a265e669c6a418f9754ad940943
Administrator <tncjs3598@naver.com>
2023-05-31 11:54:56 +0900

    update

---
+++ src/main/java/com/green/boardver3/board/model/BoardDetailVo.java
@@ -0,0 +1,10 @@
+package com.green.boardver3.board.model;
+
+public class BoardDetailVo {
+    private int iboard;
+    private String title;
+    private String ctnt;
+    private String writer;
+    private String createdAt;
+    private String updatedAt;
+}
