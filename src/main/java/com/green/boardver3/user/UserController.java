85a68eecdf54dbe40b9e41a6a5066f6859a25fd8
권민구 <roy1021@naver.com>
2023-06-02 15:27:22 +0900

    UPDATE boardpage

--- src/main/java/com/green/boardver3/user/UserController.java
+++ src/main/java/com/green/boardver3/user/UserController.java
@@ -5,7 +5,12 @@ import com.green.boardver3.user.model.UpdateDto;
 import com.green.boardver3.user.model.UserLoginDto;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.print.attribute.standard.Media;
+import java.awt.*;
 
 @RestController
 @RequestMapping("/user")
@@ -38,9 +43,16 @@ public class UserController {
     public int postLoginUser(@RequestBody UserLoginDto dto) {
         return service.login(dto);
     }
+
     @PatchMapping("/pw")
     @Operation
     public int UpdPw(@RequestBody UpdateDto dto) {
         return service.updBoard(dto);
+
+    }
+
+    @PatchMapping(name="/pic", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE })
+    public int patchPicUser(@RequestPart MultipartFile pic, @RequestParam int iuser) {
+        return 0;
     }
 }
